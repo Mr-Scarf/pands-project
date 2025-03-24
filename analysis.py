ab83052452032320b057b9a5ac10834b8d4a9ecd
Mr-Scarf <dscal07@outlook.com>
2025-03-24 20:29:13 +0000

    Initial Commit - Python Programme created + Iris Dataset added

---
+++ analysis.py
@@ -0,0 +1,29 @@
+# analysis.py
+
+# Step 3 of Project
+# 1. Outputs a summary of each variable to a single text file,  
+# 2. Saves a histogram of each variable to png files, and  
+# 3. Outputs a scatter plot of each pair of variables.  
+# 4. Performs any other analysis you think is appropriate. 
+# 
+# 
+# Author: David Scally
+
+#Instructions - to be Deleted
+
+                        #Minimum Viable Project 
+                        #  
+                        #   The minimum standard is a GitHub repository containing a README, a Python script, a 
+                        #   generated summary text file, and images. The README (and/or notebook) should contain a 
+                        #   summary of the data set and your investigations into it. It should also clearly document how 
+                        #   to run the Python code and what that code does. Furthermore, it should list all references 
+                        #   used in completing the project. 
+ 
+                        #   A better project will be well organised and contain detailed explanations. The analysis will 
+                        #   be well conceived, and examples of interesting analyses that others have pursued based on 
+                        #   the data set will be discussed. Note that the point of this project is to use Python. You may 
+                        #   use any Python libraries that you wish, whether they have been discussed in class or not. 
+                        #   You should not be thinking of using spreadsheet software like Excel to do your calculations.  
+
+
+
